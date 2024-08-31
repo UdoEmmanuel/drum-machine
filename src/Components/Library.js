f018906f46abb3b2fee22581e82f2926e2cbc330
UdoEmmanuel <obiohaemmanuel2002@gmail.com>
2024-08-31 23:17:14 +0100

    My second commit

--- src/Components/Library.js
+++ src/Components/Library.js
@@ -4,8 +4,8 @@ const Library = () => {
     
   return (
     <div class="btn-group">
-        <div id='lib-1'>Mode 1</div>
-        <div id='lib-2'>Mode 2</div>
+        <div id='Mode-1'>Mode 1</div>
+        <div id='Mode-2'>Mode 2</div>
     </div>
   )
 }
